e162770cf7d9a61aac349796e0c78edfd3a8d777
lazecoding <15062270668@163.com>
2021-04-27 21:37:58 +0800

    算法 - 查找 ：二叉查找树

--- note/sourcecode/algorithms/search/BST.java
+++ note/sourcecode/algorithms/search/BST.java
@@ -1,75 +1,12 @@
-# 查找
+package brush.algorithms.search;
 
-- 目录
-    - [二分查找](#二分查找)
-    - [二叉查找树](#二叉查找树)
-  
-经典的查找算法主要有三种高效的数据类型实现：二叉查找树、红黑树、散列表。
+import java.util.Queue;
 
-### 二分查找
-
-基于有序数组的二分查找是最简单也是最常见的查找算法。
-
-下述代码采用迭代的方式：首先将 key 和中间值做比较，如果相等返回索引；如果小于中间值则在左半部分查找；大于则在右半部分查找。
-
-```java
-public class BinarySearch {
-
-    /**
-     * 有序数组
-     */
-    private static Comparable[] keys = null;
-
-    /**
-     * 查找
-     *
-     * @param a 数组
-     * @return
-     */
-    public static int search(Comparable[] a, Comparable key) {
-        keys = a;
-        Arrays.sort(keys);
-        // 二分查找
-        int low = 0, high = keys.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int compare = keys[mid].compareTo(key);
-            if (compare == 0) {
-                return mid;
-            } else if (compare > 0) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return high;
-    }
-
-    public static void main(String[] args) {
-        Integer[] a = new Integer[]{6, 2, 1, 4, 9, 5};
-
-        System.out.println(search(a, 2));
-
-    }
-}
-```
-
-二分查找（有序链表）区别于顺序查找（基于链表），最明显的优势就是查询效率和空间需求较低，但是相比链表插入操作很慢。
-
-### 二叉查找树
-
-二叉查找树（BST）是一颗二叉树，其中每个节点都含有一个 Comparable 的键（亦可能包含相关联的值）且每个节点的键都大于其左子树的任意节点的键而小于右子树的任意节点的键。
-BST 有一个重要性质，就是它的中序遍历结果递增排序。
-
-<div align="left">
-    <img src="https://github.com/lazecoding/Note/blob/main/images/algorithms/二叉查找树值有序映射.png" width="600px">
-</div>
-
-在二叉树中查找一个键：如果树是空的，则查找未命中；如果被查找的键和根节点相等，则命中；否则我们就递归地在适当地子树中继续查找。
-
-代码实现：
-
-```java
+/**
+ * @author: lazecoding
+ * @date: 2021/4/26 22:08
+ * @description: 二叉查找树
+ */
 public class BST<Key extends Comparable<Key>, Value> {
     /**
      * 定义根节点
@@ -279,8 +216,3 @@ public class BST<Key extends Comparable<Key>, Value> {
         System.out.println(bst.get(1));
     }
 }
-```
-
-二叉查找树所有操作在最坏的情况下所需要的时间都和树的高度成正比。但是，二叉查找树在极端情况下会退化成链表，这时候查找会退化成顺序查找。
-
-
